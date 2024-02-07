422a11f150000651f1ca2e41b92e81794f057d39
Emrullah Kesken <emrullahkesken.dev@gmail.com>
2024-02-07 20:59:26 +0300

    Day 1, Assignment 2.

--- WorkingAround/Program.cs
+++ WorkingAround/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace WorkingAround
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace WorkingAround
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Person person = new Person();
+            person.FirstName = "Emrulah";
+            person.LastName = "Kesken";
+            person.NationalIdentity = 12345678911;
+            person.DateOfBirthYear = 1998;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person);
 
         }
     }
